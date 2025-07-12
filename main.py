6487092a2b55bb9b627851cad3ddae0131e9ab98
Sandeep Viswanath Reddy Yarasani <sandeepviswanathy@gmail.com>
2025-07-12 16:15:34 -0400

    Inital commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from core.config import settings
+
+# Initialize FastAPI application with settings from config
+app = FastAPI(
+    title="Choose Your Own Adventure Game API",
+    description="API for the Choose Your Own Cool Adventure games",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,  # Allow all origins for
+    # simplicity; adjust as needed
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
